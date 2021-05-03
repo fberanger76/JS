4e901903bd716f1923a4cf387c41e1caf3f7088b
Franck <franckberanger@imac-travail-de-franck.home>
2021-05-03 12:05:23 +0200

    exo 4 ok

---
+++ exo4/main.js
@@ -0,0 +1,5 @@
+let firstname = prompt ("Quel est votre prénom ?")
+let age = prompt ("Quel est votre âge ?")
+let city = prompt ("Dans quelle ville habitez-vous ?")
+
+alert("Bonjour, je m'appelle " + firstname + ", j'ai " + age +" ans et j'habite " + city);
