bc01dddf95c7149b4524f233285c96d209d067c2
Franck <franckberanger@imac-travail-de-franck.home>
2021-05-03 22:51:23 +0200

    exo 5 ok

---
+++ condition/exo5/main.js
@@ -0,0 +1,26 @@
+let day = 2;
+switch (day) {
+    case 0:
+        day = "lundi";
+        break;
+    case 1:
+        day = "mardi";
+        break;
+    case 2:
+        day = "mercredi";
+        break;
+    case 3:
+        day = "jeudi";
+        break;
+    case 4:
+        day = "vendredi";
+        break;
+    case 5:
+        day = "samedi";
+        break;
+    case 6:
+        day = "dimanche"
+        break;
+}
+
+alert("Aujourd'hui on est " + day)
