27a80efb3ac651a4131a870d6d1f00501f4369d8
Franck <franckberanger@imac-travail-de-franck.home>
2021-05-03 16:39:22 +0200

    rajout dossier exos condition + exo 1

---
+++ condition/exo1/main.js
@@ -0,0 +1,5 @@
+let firstNumber = prompt ("Choisissez un nombre");
+let secondNumber = prompt ("Choisissez un deuxième nombre");
+let resultNumber = firstNumber + secondNumber;
+
+console.log("Voici le résultat : " + (parseInt(firstNumber)+parseInt(secondNumber)) + " et voici le calcul : " + firstNumber + " +" + secondNumber );
