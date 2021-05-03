ca6b4f3d82d5150da494c7e175c605c0b151d937
Franck <franckberanger@imac-travail-de-franck.home>
2021-05-03 17:51:43 +0200

    exo 4 ok

---
+++ condition/exo4/main.js
@@ -0,0 +1,16 @@
+let note = prompt ("Choisissez une note")
+if (note >= 0 && note <= 3){
+    alert("Nul")
+}
+if (note >= 4 && note < 6){
+    alert("Moyen")
+}
+if (note >= 6 && note < 8){
+    alert("Assez Bien")
+}
+if (note >= 8 && note <= 9){
+    alert("Bien")
+}
+if (note == 10){
+    alert("Excellent")
+}
