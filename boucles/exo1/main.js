27e221d7379ca84df2b32a462fd358a9b3ac9091
Franck <franckberanger@imac-travail-de-franck.home>
2021-05-05 10:51:28 +0200

    exo 1 vraiment ok

--- boucles/exo1/main.js
+++ boucles/exo1/main.js
@@ -1,4 +1,8 @@
-var years;
-for (years = 2020; years <2031; years++) {
-    console.log(years);
+var Year;
+function IsLeapYear(Year) {
+    return (((Year & 3) == 0) && ((Year % 100 != 0) || (Year % 400 == 0)));
+   }
+for (Year=2020;Year <=2030;Year++) {
+    document.write(Year+"<br>");
+    if(IsLeapYear(Year)) document.write(" est une année bissextile<BR>".fontcolor("red"));
 }
