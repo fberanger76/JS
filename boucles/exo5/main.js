e82c00bc0db90ba3721a7f1c35d876b1508f8909
Franck <franckberanger@imac-travail-de-franck.home>
2021-05-06 11:40:26 +0200

    exo 5 ok

---
+++ boucles/exo5/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 5;
+let secondNumber = 3;
+
+while (firstNumber*secondNumber < 250) {
+
+    secondNumber = firstNumber * secondNumber;
+    console.log("the number is " + secondNumber);
+
+}
