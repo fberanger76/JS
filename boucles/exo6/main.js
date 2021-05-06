ff3ed68967dd33e8915baf83794542d99e0a333c
Franck <franckberanger@imac-travail-de-franck.home>
2021-05-06 11:46:45 +0200

    exo 6 ok

---
+++ boucles/exo6/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 9;
+let secondNumber = 2;
+
+while (firstNumber/secondNumber > 1) {
+
+    firstNumber = firstNumber / secondNumber;
+    console.log("the number is " + firstNumber);
+
+}
