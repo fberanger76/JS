22dda2a22d741267e846f3344d9d2922f04d867d
Franck <franckberanger@imac-travail-de-franck.home>
2021-05-12 10:57:54 +0200

    exo 8 corrigé

--- evenements/Exo8/main.js
+++ evenements/Exo8/main.js
@@ -1,7 +1,7 @@
 let formDuplicate = document.getElementById("formDuplicate");
 let btnValidate = document.getElementById("validate");
-let clone = formDuplicate.cloneNode(true);
 
 btnValidate.addEventListener("click", () => {
-  document.body.appendChild(clone);
+  let clone = formDuplicate.cloneNode(true);
+  document.getElementById("cloneDestination").appendChild(clone);
 });
